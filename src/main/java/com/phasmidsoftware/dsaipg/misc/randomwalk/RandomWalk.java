72082058981b686152ce2793ef002392c0790b8b
Raghavendra Prasath Sridhar <sridhar.r@northeastern.edu>
2025-01-27 19:14:41 -0500

    Submitting Assignment 1

--- src/main/java/com/phasmidsoftware/dsaipg/misc/randomwalk/RandomWalk.java
+++ src/main/java/com/phasmidsoftware/dsaipg/misc/randomwalk/RandomWalk.java
@@ -4,6 +4,8 @@
 
 package com.phasmidsoftware.dsaipg.misc.randomwalk;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Random;
 
 /**
@@ -13,43 +15,41 @@ import java.util.Random;
  * experiments can be performed to compute average distances.
  */
 public class RandomWalk {
-
     /**
      * Method to compute the distance from the origin (the lamp-post where the drunkard starts) to his current position.
      *
      * @return the (Euclidean) distance from the origin to the current position.
      */
     public double distance() {
-        // TO BE IMPLEMENTED 
-         return 0.0;
-        // END SOLUTION
+        // Use precise computation for large values of x and y
+        return Math.sqrt(BigDecimal.valueOf((long) x * x + (long) y * y).doubleValue());
     }
 
     /**
-     * Private method to move the current position, that's to say the drunkard moves
+     * Private method to move the current position, that's to say the drunkard moves.
      *
-     * @param dx the distance he moves in the x direction
-     * @param dy the distance he moves in the y direction
+     * @param dx the distance he moves in the x direction.
+     * @param dy the distance he moves in the y direction.
      */
     private void move(int dx, int dy) {
-        // TO BE IMPLEMENTED  do move
-         throw new RuntimeException("Not implemented");
-        // END SOLUTION
+        x += dx;
+        y += dy;
     }
 
     /**
-     * Perform a random walk of m steps
+     * Perform a random walk of m steps.
      *
-     * @param m the number of steps the drunkard takes
+     * @param m the number of steps the drunkard takes.
      */
     private void randomWalk(int m) {
-        // TO BE IMPLEMENTED 
-throw new RuntimeException("implementation missing");
+        for (int i = 0; i < m; i++) {
+            randomMove();
+        }
     }
 
     /**
      * Private method to generate a random move according to the rules of the situation.
-     * That's to say, moves can be (+-1, 0) or (0, +-1).
+     * Moves can be (+-1, 0) or (0, +-1).
      */
     private void randomMove() {
         boolean ns = random.nextBoolean();
@@ -59,26 +59,23 @@ throw new RuntimeException("implementation missing");
 
     private int x = 0;
     private int y = 0;
-
     private final Random random = new Random();
-
     /**
      * Perform multiple random walk experiments, returning the mean distance.
      *
-     * @param m the number of steps for each experiment
-     * @param n the number of experiments to run
-     * @return the mean distance
+     * @param m the number of steps for each experiment.
+     * @param n the number of experiments to run.
+     * @return the mean distance.
      */
     public static double randomWalkMulti(int m, int n) {
         double totalDistance = 0;
         for (int i = 0; i < n; i++) {
             RandomWalk walk = new RandomWalk();
             walk.randomWalk(m);
-            totalDistance = totalDistance + walk.distance();
+            totalDistance += walk.distance();
         }
         return totalDistance / n;
     }
-
     /**
      * The main method serves as the entry point to the RandomWalk program. It performs
      * either a single random walk experiment or several experiments, based on the
@@ -90,12 +87,9 @@ throw new RuntimeException("implementation missing");
      *             If args is empty, the method throws a RuntimeException indicating invalid syntax.
      */
     public static void main(String[] args) {
-        if (args.length == 0)
-            throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
-        int m = Integer.parseInt(args[0]);
-        int n = 30;
-        if (args.length > 1) n = Integer.parseInt(args[1]);
-        double meanDistance = randomWalkMulti(m, n);
-        System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+        for (int m = 0; m <= 150; m += 1) {
+            double meanDistance = randomWalkMulti(m, 10);
+            System.out.println(m + " steps: " + meanDistance + " over 10 experiments");
+        }
     }
 }
