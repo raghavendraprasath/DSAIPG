f0b9dfba24f010d871094043e4aee125ab3315af
Raghavendra Prasath Sridhar <sridhar.r@northeastern.edu>
2025-03-17 23:43:06 -0400

    Submitting assignment 5

--- src/main/java/com/phasmidsoftware/dsaipg/sort/par/Main.java
+++ src/main/java/com/phasmidsoftware/dsaipg/sort/par/Main.java
@@ -1,17 +1,10 @@
-/*
- * Copyright (c) 2024. Robin Hillyard
- */
-
 package com.phasmidsoftware.dsaipg.sort.par;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
 /**
@@ -20,12 +13,20 @@ import java.util.concurrent.ForkJoinPool;
  */
 public class Main {
 
+    /**
+     * The main method serves as the entry point for the program. It processes command-line arguments,
+     * configures sorting parameters, performs parallel sorting on a random array, measures execution time,
+     * and writes the performance results to a CSV file.
+     *
+     * @param args command-line arguments used for configuring program execution.
+     */
     public static void main(String[] args) {
         processArgs(args);
         System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+        performSortingTests();
         Random random = new Random();
         int[] array = new int[2000000];
-        ArrayList<Long> timeList = new ArrayList<>();
+        Collection<Long> timeList = new ArrayList<>();
         for (int j = 50; j < 100; j++) {
             ParSort.cutoff = 10000 * (j + 1);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
@@ -44,7 +45,7 @@ public class Main {
 
         }
         try {
-            FileOutputStream fis = new FileOutputStream("./src/result.csv");
+            FileOutputStream fis = new FileOutputStream("C:/Users/adity/Desktop/Spring 2025/PSA/DSAIPG/result.csv");
             OutputStreamWriter isr = new OutputStreamWriter(fis);
             BufferedWriter bw = new BufferedWriter(isr);
             int j = 0;
@@ -61,12 +62,29 @@ public class Main {
         }
     }
 
+    /**
+     * Processes the command-line arguments by iterating through the provided array of arguments.
+     * Each argument is checked for specific prefixes (e.g., "-" symbols), and arguments with such prefixes
+     * are further handled using {@link #processArg(String[])}. The method continuously modifies the arguments array
+     * by removing processed elements.
+     *
+     * @param args an array of strings representing command-line arguments to be processed.
+     *             Each argument can include options, flags, or parameters that configure the program's behavior.
+     */
     private static void processArgs(String[] args) {
         String[] xs = args;
         while (xs.length > 0)
             if (xs[0].startsWith("-")) xs = processArg(xs);
     }
 
+    /**
+     * Processes a given array of strings, extracting a subset of elements and applying a command
+     * processing operation on the first two elements of the input array.
+     *
+     * @param xs the input array of strings where the first two elements are used for command processing
+     *           and the remaining elements are returned as the result.
+     * @return an array of strings containing the elements of the input array excluding the first two.
+     */
     private static String[] processArg(String[] xs) {
         String[] result = new String[0];
         System.arraycopy(xs, 2, result, 0, xs.length - 2);
@@ -74,14 +92,42 @@ public class Main {
         return result;
     }
 
+    /**
+     * Processes a command and performs an associated action based on the given inputs.
+     *
+     * @param x the command identifier, which specifies the operation to perform.
+     *          Supported values: "N" for setting configuration and "P" for retrieving
+     *          the common pool parallelism level.
+     * @param y the value associated with the command. For "N", this represents the
+     *          configuration value to be set.
+     */
     private static void processCommand(String x, String y) {
-        if (x.equalsIgnoreCase("N")) setConfig(x, Integer.parseInt(y));
-        else
-            // TODO sort this out
-            if (x.equalsIgnoreCase("P")) //noinspection ResultOfMethodCallIgnored
-                ForkJoinPool.getCommonPoolParallelism();
+        if (x.equalsIgnoreCase("N")) {
+            setConfig(x, Integer.parseInt(y));
+        } else if (x.equalsIgnoreCase("P")) {
+            int parallelismLevel = ForkJoinPool.getCommonPoolParallelism();
+            System.out.println("Current common pool parallelism: " + parallelismLevel);
+        } else if (x.equalsIgnoreCase("-cutoff")) {
+            try {
+                int newCutoff = Integer.parseInt(y);
+                ParSort.cutoff = newCutoff;
+                System.out.println("Cutoff set to: " + newCutoff);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid cutoff value provided. Using default.");
+            }
+        } else {
+            System.out.println("Unknown command or not implemented: " + x);
+        }
     }
 
+    /**
+     * Configures a key-value pair in the application's configuration.
+     * This method stores the specified key and associated integer value
+     * into the configuration map.
+     *
+     * @param x the key to be stored in the configuration
+     * @param i the integer value to be associated with the specified key
+     */
     private static void setConfig(String x, int i) {
         configuration.put(x, i);
     }
@@ -89,5 +135,29 @@ public class Main {
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
     private static final Map<String, Integer> configuration = new HashMap<>();
 
+    private static void performSortingTests() {
+        Random random = new Random();
+        int[] sizes = {100000, 500000, 1000000, 2000000, 5000000};
 
+        for (int size : sizes) {
+            int[] array = new int[size];
+            Collection<Long> timeList = new ArrayList<>();
+
+            for (int j = 50; j < 100; j++) {
+                ParSort.cutoff = 10000 * (j + 1);
+                long totalTimes = 0;
+                for (int t = 0; t < 10; t++) {
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = random.nextInt(10000000);
+                    }
+                    long startTime = System.currentTimeMillis();
+                    ParSort.sort(array, 0, array.length);
+                    long endTime = System.currentTimeMillis();
+                    totalTimes += (endTime - startTime);
+                }
+                timeList.add(totalTimes / 10);
+                System.out.println("Array Size: " + size + ", Cutoff: " + ParSort.cutoff + ", Average Time: " + (totalTimes / 10) + "ms");
+            }
+        }
+    }
 }
--- src/main/java/com/phasmidsoftware/dsaipg/sort/par/ParSort.java
+++ src/main/java/com/phasmidsoftware/dsaipg/sort/par/ParSort.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2024. Robin Hillyard
- */
-
 package com.phasmidsoftware.dsaipg.sort.par;
 
 import java.util.Arrays;
@@ -16,6 +12,7 @@ import java.util.concurrent.CompletableFuture;
  * @author Ziyao Qiao. Thanks very much.
  */
 final class ParSort {
+
 
     /**
      * Specifies the cutoff value used to determine when to switch from parallel sorting
@@ -40,16 +37,32 @@ final class ParSort {
      */
     public static void sort(int[] array, int from, int to) {
         if (to - from >= cutoff) {
-            CompletableFuture<int[]> completableFuture1 = null;
-            CompletableFuture<int[]> completableFuture2 = null;
-            // TO BE IMPLEMENTED 
-            // END SOLUTION
+            int mid = from + (to - from) / 2;
+
+            CompletableFuture<int[]> completableFuture1 = CompletableFuture.supplyAsync(() -> {
+                return sortRecursive(array, from, mid);
+            });
+
+            CompletableFuture<int[]> completableFuture2 = CompletableFuture.supplyAsync(() -> {
+                return sortRecursive(array, mid, to);
+            });
+
             CompletableFuture<int[]> completableFuture = completableFuture1.thenCombine(completableFuture2, ParSort::doMerge);
-            completableFuture.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
+            completableFuture.whenComplete((result, throwable) -> {
+                if (throwable != null) {
+                    throwable.printStackTrace();
+                } else {
+                    System.arraycopy(result, 0, array, from, result.length);
+                }
+            });
+
+
             completableFuture.join();
-        } else
+        } else {
             Arrays.sort(array, from, to);
+        }
     }
+
 
     /**
      * Recursively sorts a specified portion of the input array and returns a new sorted array.
@@ -63,9 +76,8 @@ final class ParSort {
      */
     static int[] sortRecursive(int[] array, int from, int to) {
         int[] result = new int[to - from];
-        // TO BE IMPLEMENTED 
-         // NOTE you need to do something here so that result is the sorted version of array.
-        // END SOLUTION
+        System.arraycopy(array, from, result, 0, result.length);
+        Arrays.sort(result);
         return result;
     }
 
@@ -88,6 +100,7 @@ final class ParSort {
             else if (xs2[j] < xs1[i]) result[k] = xs2[j++];
             else result[k] = xs1[i++];
         }
+
         return result;
     }
 
@@ -95,7 +108,6 @@ final class ParSort {
      * Asynchronously sorts the specified portion of the input array using a parallel sorting algorithm.
      * This method extracts a subsection of the given array, sorts it, and returns a CompletableFuture
      * containing the sorted portion of the array.
-     *
      * @param array the input array to extract and sort
      * @param from  the starting index (inclusive) of the portion of the array to be sorted
      * @param to    the ending index (exclusive) of the portion of the array to be sorted
